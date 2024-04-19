7d1b36a7c869d168a636df00e019ed74b8f406b2
heidashuai777 <113970718+heidashuai777@users.noreply.github.com>
2024-04-19 10:12:18 +0800

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Description: Main file to run the optimization algorithm
+# Importing the necessary packages
 import matplotlib.pyplot as plt
 from RUN import RUN
 from BenchmarkFunctions import BenchmarkFunctions
@@ -16,7 +18,7 @@ lb, ub, dim, fobj = BenchmarkFunctions(Func_name)
 
 Best_fitness, BestPositions, Convergence_curve = RUN(nP, MaxIt, lb, ub, dim, fobj)
 
-# Draw objective space
+# Draw the convergence curve
 plt.figure()
 plt.semilogy(Convergence_curve, color='r', linewidth=2)
 plt.title('Convergence curve')
